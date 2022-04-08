32852a13b7a274c99204168cada897554cb49942
Eli Wood <mreliwood1@gmail.com>
2022-04-07 20:39:06 -0700

    highlight text area based on current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,75 @@
 var currentDay = moment();
 $("#currentDay").text(currentDay.format("dddd, MMMM Do"));
 
-// listen for save button clicks
-// for (let i = 0; i < 10; i++) {
-//     $(`saveBtn${i}`).click(saveComment());;
-// }
-
 // save comments to local storage
 
-// for (let i = 1; i < 10; i++) {
+for (let i = 0; i < 10; i++) {
 
-//     var saveBtn = $(`#saveBtn${i}`);
-//     var comment = $(`#comment${i}`).val();
+    var saveBtn = $(`#saveBtn${i}`);
+    var comment = $(`#nine`).val();
 
-//     console.log(comment)
-
-//     saveBtn.click(function() {
-//         console.log(comment)
-//         localStorage.setItem(`comment${i}`, comment)
-//     });
+    saveBtn.click(function(event) {
+        event.preventDefault();
+        console.log(comment)
+        localStorage.setItem(`comment${i}`, comment);
+    });
     
-// };
+};
 
+// highlight schedule based on time
+var currentTime = moment().format('H');
 
-var nine = 
+var allHours = [
+    {
+        time: 9,
+        id: $('#nine')
+    },
+    {
+        time: 10,
+        id: $('#ten')
+    },
+    {
+        time: 11,
+        id: $('#eleven')
+    },
+    {
+        time: 12,
+        id: $('#twelve')
+    },
+    {
+        time: 13,
+        id: $('#thirteen')
+    },
+    {
+        time: 14,
+        id: $('#fourteen')
+    },
+    {
+        time: 15,
+        id: $('#fifteen')
+    },
+    {
+        time: 16,
+        id: $('#sixteen')
+    },
+    {
+        time: 17,
+        id: $('#seventeen')
+    }
+];
+
+for (let i = 0; i < allHours.length; i++) {
+
+    var hourId = allHours[i].id;
+
+    if (currentTime < allHours[i].time) {
+        hourId.addClass('bg-success');
+    } else if (currentTime === allHours[i].time) {
+        hourId.removeClass('bg-success');
+        hourId.addClass('bg-danger');
+    } else {
+        hourId.removeClass('bg-danger');
+        hourId.addClass('bg-light');
+    };
+
+}
